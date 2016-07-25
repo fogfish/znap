2e31714c144cea4669e42e8817bb9ee240ed152b
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2016-07-25 15:08:31 +0300

    Write data and offsets to disk

---
+++ src/main/scala/org/zalando/znap/disk/OffsetWriter.scala
@@ -0,0 +1,31 @@
+package org.zalando.znap.disk
+
+import java.io.{BufferedWriter, File, FileWriter, IOException}
+
+import akka.actor.{Actor, ActorLogging}
+import org.zalando.znap.nakadi.Messages.Ack
+import org.zalando.znap.utils.{EscalateEverythingSupervisorStrategy, NoUnexpectedMessages}
+
+private class OffsetWriter(val root: File) extends Actor with NoUnexpectedMessages with ActorLogging {
+  import OffsetWriter._
+
+  override def receive: Receive = {
+    case WriteOffset(partition, offset) =>
+      val partitionFile = new File(root, s"partition_$partition")
+      try
+      {
+        val out = new BufferedWriter(new FileWriter(partitionFile))
+        out.write(offset)
+        out.close()
+      } catch {
+        case e: IOException =>
+          log.error(e, s"Error writing offset $offset")
+          throw e
+      }
+      sender() ! Ack
+  }
+}
+
+object OffsetWriter {
+  final case class WriteOffset(partition: String, offset: String)
+}
